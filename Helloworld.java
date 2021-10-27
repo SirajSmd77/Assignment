c9310d83515dca4e42ced8bd9210b1fc90a2a71d
SirajSmd77 <86145197+SirajSmd77@users.noreply.github.com>
2021-10-27 19:32:42 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package firstHand_oct_26;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		System.out.print("Welcome to Geekster");
+	}
+
+}
