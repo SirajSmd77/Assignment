e13e5dae167ea2b2885a60fae8389babced24313
SirajSmd77 <86145197+SirajSmd77@users.noreply.github.com>
2021-10-29 17:33:29 +0530

    Add files via upload

---
+++ TableofAllNumbers.java
@@ -0,0 +1,15 @@
+package firstHand_oct_27;
+
+public class TableofAllNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int j=1;j<=10;j+=1) {
+
+			for(int i =1;i<=10;i++){
+				 System.out.println(j+"*"+i+"="+j*i);	 
+			 }	
+		}
+	}
+
+}
