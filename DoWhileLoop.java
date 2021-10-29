4c9154fa7f4d47ac184a65bf60ac5336c9cf369a
SirajSmd77 <86145197+SirajSmd77@users.noreply.github.com>
2021-10-29 17:31:04 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,15 @@
+package firstHand_oct_27;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		do {
+			System.out.println(num);
+			num+=1;
+		}while(num<=10);
+	
+	}
+
+}
